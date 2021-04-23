807ab41fb894c39a14d9001ddcfbd1eb8dd27e03
Daniel B. Hill <daniel@gluo.nz>
2021-04-23 20:04:04 +1200

    fix build script and .mod

--- build.sh
+++ build.sh
@@ -1,13 +1,18 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+build() {
+
+        7z a ../Compass.zip *
+        mv ../Compass.zip ../Compass.mod
+        cp ../Compass.mod ./
+}
 
 loop() {
     while read dir events fname; do
         echo "file change $fname; rebuilding"
-        7z a ../Compass.zip *
-        mv ../Compass.zip ../Compass.mod
+        build
     done
 }
-
+build
 inotifywait -q -e close_write -rm . | loop
